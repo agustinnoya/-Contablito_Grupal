2c8cd9a9a5750ba702f392c651b944025a8ef82c
Nahuel Pereira <110473467+nahuper@users.noreply.github.com>
2022-08-19 11:40:10 -0300

    parrafo totalVentas y variables en JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ const minimo = document.getElementById("minimo")
 const exento = document.getElementById("exento")
 const subtotal = document.getElementById("subtotal")
 const boton = document.getElementById("button")
-const totalventas = 
-const totalcompras =
-const textofinal = "Total Ventas: $" + totalventas + "Total Compras: $" + totalcompras
+const totalventas = document.getElementById("totalventas");
+const totalcompras = document.getElementById("totalcompras");
+const textofinal = "Total Ventas: $" + totalventas + "Total Compras: $" + totalcompras;
+
